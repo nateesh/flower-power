79333d3d0ef2410dfe59a0b729b04ea83b0832c6
nateesh <nathan.eshraghi@gmail.com>
2022-09-25 08:53:16 +1000

    create model file, opening notes

---
+++ model.py
@@ -0,0 +1,6 @@
+
+
+# ----- This is what Fred was talking about in the tutorial
+# To rescale an input in the [0, 255] range to be 
+# in the [0, 1] range, you would pass scale=1./255.
+# https://keras.io/api/layers/preprocessing_layers/image_preprocessing/rescaling/#rescaling-class
